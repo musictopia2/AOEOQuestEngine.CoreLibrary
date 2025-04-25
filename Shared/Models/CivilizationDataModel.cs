fefcecfc9fdba2e7d271d14e1019f75175633bbb
Andy Walter <musictopia2@hotmail.com>
2025-04-25 13:11:35 -0700

    Add CivilizationDataModel class with properties
    Introduced a new `CivilizationDataModel` class in the
    `AOEOQuestEngine.CoreLibrary.Shared.Models` namespace.
    This class features three properties: `CivAbb` and `CivTitle`
    as strings initialized to empty values, and
    `AverageCompletionMinuteTime` as an integer to track
    average completion time, with 0 indicating not completed.

---
+++ Shared/Models/CivilizationDataModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AOEOQuestEngine.CoreLibrary.Shared.Models;
+public class CivilizationDataModel
+{
+    public string CivAbb { get; set; } = "";
+    public string CivTitle { get; set; } = ""; //this way a process can run which can list civs.
+    public int AverageCompletionMinuteTime { get; set; } //0 means not done.
+}
