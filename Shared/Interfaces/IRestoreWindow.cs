20fccfa5a4f1014867d527ecbe34a94c7ee282f5
Andy Walter <musictopia2@hotmail.com>
2025-04-25 17:39:48 -0700

    Add IRestoreWindow interface and NoOpRestoreWindow class
    Introduced two new files: `IRestoreWindow.cs` defines an interface with a `RestoreWindow()` method for window restoration. `NoOpRestoreWindow.cs` implements this interface with an empty method, serving as a no-operation placeholder.

---
+++ Shared/Interfaces/IRestoreWindow.cs
@@ -0,0 +1,6 @@
+﻿namespace AOEOQuestEngine.CoreLibrary.Shared.Interfaces;
+public interface IRestoreWindow
+{
+    //so if a window needs to be restored, then somebody needs to figure out how to restore the window.
+    void RestoreWindow();
+}
---
+++ Shared/Services/NoOpRestoreWindow.cs
@@ -0,0 +1,8 @@
+﻿namespace AOEOQuestEngine.CoreLibrary.Shared.Services;
+public class NoOpRestoreWindow : IRestoreWindow
+{
+    void IRestoreWindow.RestoreWindow()
+    {
+        
+    }
+}
