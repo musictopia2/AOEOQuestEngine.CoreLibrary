d112d86d8f82e91280d24ad1543d11e5c6170965
Andy Walter <musictopia2@hotmail.com>
2025-06-02 06:59:42 -0700

    Add IQuestResultPersistenceService interface
    Introduces a new interface `IQuestResultPersistenceService`
    in the `AOEOQuestEngine.CoreLibrary.Shared.Interfaces` namespace.
    This interface includes three asynchronous methods:
    `SaveAsync`, `LoadPendingAsync`, and `ClearPendingAsync`
    for managing quest result data.

---
+++ Shared/Interfaces/IQuestResultPersistenceService.cs
@@ -0,0 +1,7 @@
+﻿namespace AOEOQuestEngine.CoreLibrary.Shared.Interfaces;
+public interface IQuestResultPersistenceService
+{
+    Task SaveAsync(QuestResultModel result);
+    Task<QuestResultModel?> LoadPendingAsync();
+    Task ClearPendingAsync();
+}
