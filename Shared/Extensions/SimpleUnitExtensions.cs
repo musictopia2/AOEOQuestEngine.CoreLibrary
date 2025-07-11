070b022afea671a03c4d4fb5b35c5363be283244
Andy Walter <musictopia2@hotmail.com>
2025-07-11 12:32:45 -0700

    Added new extension for adding standard units.

--- Shared/Extensions/SimpleUnitExtensions.cs
+++ Shared/Extensions/SimpleUnitExtensions.cs
@@ -2,6 +2,10 @@
 public static class SimpleUnitExtensions
 {
     //must be public now since its now possible to use outside for experimenting.
+    /// <summary>
+    /// Adds a full custom unit XML element to the entire list of units.
+    /// This appends the unit definition directly under the root unit collection element.
+    /// </summary>
     public static void AddCustomUnitToEntireList(this XElement source, string details)
     {
         XElement extras = XElement.Parse(details);
---
+++ Shared/Extensions/UnitHandlerExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AOEOQuestEngine.CoreLibrary.Shared.Extensions;
+public static class UnitHandlerExtensions
+{
+    public static void RegisterStandardUnit<T>(this T handler, XElement root)
+        where T : IUnitHandler
+    {
+        var unitElement = root.Elements()
+           .SingleOrDefault(x => (string?)x.Attribute("name") == T.SupportedProtoName);
+        if (unitElement == null)
+        {
+            // Optional: log warning
+            return;
+        }
+        var trainPoints = unitElement.Element("TrainPoints");
+        if (trainPoints != null)
+        {
+            trainPoints.Value = "0.0000";
+        }
+    }
+}
